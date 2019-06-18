b8554998f273c67a9db9a6013ab162100301c514
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2019-06-18 14:22:31 -0400

    move utilities to utils.R

--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,30 @@ get_value.default <- function(x, ...){
 get_value.reactive <- function(x, ...){
   x()
 }
+
+
+date_range_presets_vec <- function(max_date = NULL){
+  weeks_back <- c(1, 2, 4, 8, 12, 26, 365 / 7, 10*365 / 7)
+  dates <- as.character(Sys.Date() - as.integer(weeks_back*7))
+  weeks_back <- as.integer(weeks_back)
+  names(dates) <- dplyr::case_when(
+    weeks_back == 1 ~ "Last Week",
+    weeks_back <= 12 ~ stringr::str_c("Last ", weeks_back, " Weeks"),
+    weeks_back == 26 ~ "Last 6 Months",
+    weeks_back == 52 ~ "Last Year",
+    weeks_back > 52 ~ "All Time"
+  )
+  if (!is.null(max_date)) {
+    dates <- dates[dates <= max_date]
+  }
+  dates
+}
+
+set_selected <- function(x, choices, default){
+  if (!is.null(x) && (x %in% choices)){
+    x
+  } else {
+    default
+  }
+}
+
