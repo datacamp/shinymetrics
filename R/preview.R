5bc26a6a4b613ce51518caf9d751466989594eab
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2019-06-24 12:49:41 -0700

    simplify examples for preview_metric

--- R/preview.R
+++ R/preview.R
@@ -5,26 +5,20 @@
 #' @examples
 #' library(dplyr)
 #' \dontrun{
-#' metric_satisfaction <- datacampr::tbl_metric_product_survey_avg_satisfaction()
-#' preview_metric({
-#'   metric_satisfaction %>%
-#'   filter(period == 'rolling_28d')
-#' })
-#' metric <- datacampr::tbl_metric_content_courses_avg_rating_wtd()
-#' preview_metric({
-#'   metric %>%
-#'     filter(period == 'rolling_28d')
-#' })
-#' preview_metric({
-#'   d <- metric %>%
-#'     filter(period == 'rolling_28d') %>%
-#'     filter(launch_status == "live") %>%
-#'     filter(technology == "R")
-#' })
+#'  metric_satisfaction <- datacampr::tbl_metric_product_survey_avg_satisfaction()
+#'  preview_metric(metric_satisfaction)
+#'  metric_rating <- datacampr::tbl_metric_content_courses_avg_rating_wtd()
+#'  preview_metric(metric_rating, selected_period = 'Rolling 28 Day')
+#'  preview_metric(
+#'    metric %>%
+#'      filter(launch_status == "live") %>%
+#'      filter(technology == "R"),
+#'    selected_period = 'Rolling 28 Day'
+#'  )
 #' }
 #' @param metric a \code{tbl_metric} object
 #' @param ... additional parameters passed to \code{metric_panel}
 #' @export
 preview_metric <- function(metric, ...){
-  shinybones::preview_module('metric_panel', metric = metric, ...)
+  shinybones::preview_module(metric_panel, metric = metric, ...)
 }
